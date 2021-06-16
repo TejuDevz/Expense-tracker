dc15bbe7dc66df9b619eafa68f2b8fcb9c76effd
Teju Devz <Tejudevelopz16@gmail.com>
2021-06-16 20:51:43 +0530

    Expense List Static completed

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import Expenses from "./components/Expenses";
 
 function App() {
+  const expenses = [
+    {
+      id: "e1",
+      title: "Book on Einstein's relativity",
+      amount: 206,
+      date: new Date(2021, 2, 12),
+    },
+    {
+      id: "e2",
+      title: "Disney+ Premium",
+      amount: 306,
+      date: new Date(2021, 6, 13),
+    },
+    {
+      id: "e3",
+      title: "Quantum Mechanincs",
+      amount: 399,
+      date: new Date(2021, 5, 13),
+    },
+    {
+      id: "e4",
+      title: "MERN stack Guide",
+      amount: 299,
+      date: new Date(2021, 5, 15),
+    },
+  ];
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Let's Get Started</h1>
+      <Expenses items={expenses} />
     </div>
   );
 }
